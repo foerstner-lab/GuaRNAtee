efbc43e8ff4ab257bc49f68cdbc3a43d1d7d7f0b
elhossary <muhammad_elhossary@hotmail.com>
2026-03-11 10:46:36 +0100

    remove guarnatee_v2.py

--- run.sh
+++
@@ -1,8 +0,0 @@
-python guarnatee.py \
---gffs inputs/annotations/A_baumannii_ATCC_17978.gff \
---fastas inputs/reference_sequences/A_baumannii_ATCC_17978.fa \
---out_dir output \
---wigs inputs/wigs/A_baumannii_ATCC_17978_OD_0.2_rep1_p1_reverse.wig:OD_0.2:1:r:P1 \
---wigs inputs/wigs/A_baumannii_ATCC_17978_OD_0.2_rep1_p2_reverse.wig:OD_0.2:1:r:P2 \
---wigs inputs/wigs/A_baumannii_ATCC_17978_OD_0.2_rep1_p1_forward.wig:OD_0.2:1:f:P1 \
---wigs inputs/wigs/A_baumannii_ATCC_17978_OD_0.2_rep1_p2_forward.wig:OD_0.2:1:f:P2
